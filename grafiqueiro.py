a714bdfab4aaf191eb8d066a281cb553d084e5fe
cacrespo <lvccrespo@gmail.com>
2015-02-11 10:26:39 -0300

    Planificando desarrollo del código...

--- grafiqueiro.py
+++ grafiqueiro.py
@@ -1,5 +1,24 @@
-# Cuerpo del programa
+#
+# GRAFIQUEIRO V0.1
+#
+# AUTOR: Carlos A. Crespo
+# FECHA: febrero de 2015
 
 # Módulos para cargar archivos XLSX / PPTX
+#
+#
+
+
+
+# Cuerpo del programa
+
+Class PPT:
+  def __init__(sef,ruta):
+    pass
+
+
+
+
+
 
 # funciones : carga presentación, carga planilla de datos, carga archivo de comandos, comandos.
