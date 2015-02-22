076bbd7185ad2ba934c2546871d8b84e124311f0
cacrespo <lvccrespo@gmail.com>
2015-02-22 20:58:49 -0300

    Update grafiqueiro.py

--- grafiqueiro.py
+++ grafiqueiro.py
@@ -1,16 +1,18 @@
+# -*- coding: utf-8 -*-
 #
 # GRAFIQUEIRO V0.1
 #
 # AUTOR: Carlos A. Crespo
 # FECHA: febrero de 2015
-
 # Módulos para cargar archivos XLSX / PPTX
+# Dependencias: python-pptx, openpyxl
+
 from pptx import *
+from openpyxl import *
+
 #
-
-
 # Cuerpo del programa
-
+#
 
 class Grafiqueiro:
 
@@ -25,29 +27,53 @@ class Grafiqueiro:
         '''Crea el objeto PPTX y establece texto de ruta de archivo de salida'''
         self.ppt=Presentation(ruta)
         self.ruta_salida = ruta[0:-5] + "_salida.pptx"
-        pass
+
+    def abrir_xls(self,ruta):
+        ''' Crea el objeto XLSX'''
+        self.xls=load_workbook(ruta).get_sheet_by_name("Datos")
+
 
     def carga_shape(self,b,c,d):
-        # self.ppt = Presentacion
-        # b = N° de Slide
-        # c = Texto
-        # d = Nombre del objeto
+        '''Carga shape con el valor de una celda
+        self.ppt = Presentacion
+        b = N° de Slide
+        c = Texto (celda entre comillas y en mayúsculas)
+        d = Nombre del objeto'''
         for j in range(0,len(self.ppt.slides[b-1].shapes)):
             if self.ppt.slides[b-1].shapes[j].name == d:
-                self.ppt.slides[b-1].shapes[j].text = c
+                self.ppt.slides[b-1].shapes[j].text = self.xls[c.upper()].value
         self.ppt.save(self.ruta_salida)
 
-def correr_syntax(ruta):
-  pass
+    def carga_grafico(self,b,c,d):
+        '''Carga grafico con valores de un rango de celdas
+        self.ppt = Presentacion
+        b = N° de Slide
+        c = Rango de celdas
+        d = Nombre del objeto'''
+        for j in range(0,len(self.ppt.slides[b-1].shapes)):
+            if self.ppt.slides[b-1].shapes[j].name == d:
+                #print self.ppt.slides[b-1].shapes[j].table
+                pass
 
-def abrir_xls(ruta):
-  pass
+    def carga_tabla(self,b,c,d):
+        '''Carga grafico con valores de un rango de celdas
+        self.ppt = Presentacion
+        b = N° de Slide
+        c = Rango de celdas
+        d = Nombre del objeto'''
+        pass
 
-def carga_grafico():
-  pass
+    def carga_caja():
+        pass
 
-def carga_tabla():
-  pass
+
+
+
+
+    def correr_syntax(ruta):
+        pass
+
+
 
 
 
