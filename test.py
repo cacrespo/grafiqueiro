36ffa3833fd81b028f79e69a03910af221dca87e
cacrespo <lvccrespo@gmail.com>
2015-05-29 18:55:34 -0300

    Update test.py

--- test.py
+++ test.py
@@ -8,3 +8,7 @@ datos.categories =  ['West', 'East', 'North', 'South', 'Other']
 datos.add_series('Series 1', (0.135, 0.324, 0.180, 0.235, 0.126))
 datos.add_series('Series 2', (0.5, 0.34, 0.80, 0.235, 0.126))
 datos.add_series('Series 3', (0.1, 0.34, 0.80, 0.235, 0.126))
+
+
+ txt = open("C:\Users\carlosc\Desktop\Grafiqueiro\prueba.txt")
+>>> exec("prueba."+str(txt.read()))
